084c96246292924a5a0b48713d3504be789c4004
Will Medders <wmedders@gmail.com>
2021-11-11 16:00:02 -0600

    Add house to model_b

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+house = {
+  "style" => "ranch",
+  "construction" => "brick",
+  "square footage" => 2150,
+  "lot size" => 0.33,
+  "gas appliances" => false,
+  "fireplace" => true,
+  "bed/bath count" => [3, 2],
+  "city/state" => ["Mobile", "Alabama"]
+}
