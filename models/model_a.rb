476c1c85fbf0a81ae777fbbaf367219c2ccad4c1
Will Medders <wmedders@gmail.com>
2021-11-11 15:51:39 -0600

    Create model files

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+laptop = {
+  "brand" => "Apple",
+  "model" => "MacBook Pro",
+  "year" => 2020,
+  "screen size" => 13.0,
+  "64 bit" => true,
+  "SSD" => true,
+  "ports" => ["Thunderbolt #1", "Thunderbolt #2", "3.5 audio"],
+  "input devices" => ["keyboard", "trackpad"]
+}
