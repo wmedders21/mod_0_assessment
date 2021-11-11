e51228f138f1e6159dbd898dbdf0384bfe361c23
Will Medders <wmedders@gmail.com>
2021-11-11 15:44:03 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,17 @@
+animal = "Gecko"
+
+is_cuddly = false
+
+animal = "Porcupine"
+
+num_8 = 16
+
+num_3 = 25
+
+quotient = num_3 / num_8
+
+favorite_films = ["Lawrence of Arabia", "20,000 Leagues Under the Sea", "Encino Man"]
+
+favorite_films.delete_at(0)
+
+puts animal.length
